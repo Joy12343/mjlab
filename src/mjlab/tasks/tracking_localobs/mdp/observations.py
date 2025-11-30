e94ec16dc2a1fed74e66f947a8f34c86f23c1852
Joy12343 <jzhuo1919@gmail.com>
2025-11-29 19:29:59 -0500

    Initial clean commit

---
+++ src/mjlab/tasks/tracking_localobs/mdp/observations.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, cast
+
+import torch
+
+from mjlab.utils.lab_api.math import (
+  matrix_from_quat,
+  subtract_frame_transforms,
+)
+
+from .commands import MotionCommand
+
+if TYPE_CHECKING:
+  from mjlab.envs import ManagerBasedRlEnv
+
+
+def motion_anchor_pos_b(env: ManagerBasedRlEnv, command_name: str) -> torch.Tensor:
+  command = cast(MotionCommand, env.command_manager.get_term(command_name))
+
+  pos, _ = subtract_frame_transforms(
+    command.robot_anchor_pos_w,
+    command.robot_anchor_quat_w,
+    command.anchor_pos_w,
+    command.anchor_quat_w,
+  )
+
+  return pos.view(env.num_envs, -1)
+
+
+def motion_anchor_ori_b(env: ManagerBasedRlEnv, command_name: str) -> torch.Tensor:
+  command = cast(MotionCommand, env.command_manager.get_term(command_name))
+
+  _, ori = subtract_frame_transforms(
+    command.robot_anchor_pos_w,
+    command.robot_anchor_quat_w,
+    command.anchor_pos_w,
+    command.anchor_quat_w,
+  )
+  mat = matrix_from_quat(ori)
+  return mat[..., :2].reshape(mat.shape[0], -1)
+
+
+def robot_body_pos_b(env: ManagerBasedRlEnv, command_name: str) -> torch.Tensor:
+  command = cast(MotionCommand, env.command_manager.get_term(command_name))
+
+  num_bodies = len(command.cfg.body_names)
+  pos_b, _ = subtract_frame_transforms(
+    command.robot_anchor_pos_w[:, None, :].repeat(1, num_bodies, 1),
+    command.robot_anchor_quat_w[:, None, :].repeat(1, num_bodies, 1),
+    command.robot_body_pos_w,
+    command.robot_body_quat_w,
+  )
+
+  return pos_b.view(env.num_envs, -1)
+
+
+def robot_body_ori_b(env: ManagerBasedRlEnv, command_name: str) -> torch.Tensor:
+  command = cast(MotionCommand, env.command_manager.get_term(command_name))
+
+  num_bodies = len(command.cfg.body_names)
+  _, ori_b = subtract_frame_transforms(
+    command.robot_anchor_pos_w[:, None, :].repeat(1, num_bodies, 1),
+    command.robot_anchor_quat_w[:, None, :].repeat(1, num_bodies, 1),
+    command.robot_body_pos_w,
+    command.robot_body_quat_w,
+  )
+  mat = matrix_from_quat(ori_b)
+  return mat[..., :2].reshape(mat.shape[0], -1)
+
+
+def robot_body_lin_vel_b(env: ManagerBasedRlEnv, command_name: str) -> torch.Tensor:
+    command = cast(MotionCommand, env.command_manager.get_term(command_name))
+    rotm_T = matrix_from_quat(command.robot_anchor_quat_w).transpose(-1, -2)
+    rel_vel = command.robot_body_lin_vel_w - command.robot_anchor_lin_vel_w[:, None, :]
+
+    vel_b = torch.einsum("bij,bmj->bmi", rotm_T, rel_vel)
+
+    return vel_b.reshape(env.num_envs, -1)
+
+
+
+def robot_body_ang_vel_b(env: ManagerBasedRlEnv, command_name: str) -> torch.Tensor:
+    command = cast(MotionCommand, env.command_manager.get_term(command_name))
+    rotm = matrix_from_quat(command.robot_anchor_quat_w)
+    rotm_T = rotm.transpose(-1, -2)
+
+    ang_vel_b = torch.einsum(
+        "bij,bmj->bmi",
+        rotm_T,
+        command.robot_body_ang_vel_w,
+    )
+    return ang_vel_b.reshape(env.num_envs, -1)
